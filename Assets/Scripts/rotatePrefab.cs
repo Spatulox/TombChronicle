8b0dfdd42d536bdd074301e81276fdb22064004c
Spatulox <M78stormtrooper@laposte.net>
2024-01-05 22:17:15 +0100

    update rotatePrefab

--- Assets/Scripts/rotatePrefab.cs
+++ Assets/Scripts/rotatePrefab.cs
@@ -24,25 +24,29 @@ public class RotatePrefab : MonoBehaviour
     private IEnumerator RotateCoroutine()
     //void Update()
     {
+	    
+	    Quaternion startRotation = prefab.transform.rotation;
+	    //Quaternion endRotation = Quaternion.identity;
+	    
 	    if (axeToRotate == "x")
 	    {
-		    _endRotation = Quaternion.Euler(degreeToRotate, 0, 0);
+		    _endRotation = Quaternion.Euler(degreeToRotate, 0, 0) * startRotation;
 		    //prefab.transform.Rotate(new Vector3(degreeToRotate,0,0), Space.Self);    
 	    }
 	    else if (axeToRotate == "y")
 	    {
-		    _endRotation = Quaternion.Euler(0, degreeToRotate, 0);
+		    _endRotation = Quaternion.Euler(0, degreeToRotate, 0) * startRotation;
 		    //prefab.transform.Rotate(new Vector3(0,degreeToRotate,0), Space.Self);
 	    }
 	    else if (axeToRotate == "z")
 	    {
-		    _endRotation = Quaternion.Euler(0, 0, degreeToRotate);
+		    _endRotation = Quaternion.Euler(0, 0, degreeToRotate) * startRotation;
 		    //prefab.transform.Rotate(new Vector3(0,0,degreeToRotate), Space.Self);
 	    }
 	    
 	    
 	    float t = 0f;
-	    Quaternion startRotation = prefab.transform.rotation;
+	    //Quaternion startRotation = prefab.transform.rotation;
 
 	    while (t < 1) {
 		    t += Time.deltaTime / timeToRotate;
