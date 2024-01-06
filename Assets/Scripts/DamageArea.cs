2f98ec6d44640b7d78a748aeb9b736c99d017b64
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-06 01:18:23 +0100

    Update DamageArea.cs (Life)

--- Assets/Scripts/DamageArea.cs
+++ Assets/Scripts/DamageArea.cs
@@ -2,16 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DamageArea : MonoBehaviour
 
+
 {
     public  LifeSystem life;
+    public Text lifeText;
+    
 
         // Start is called before the first frame update
+
     void Start()
     {
-        
+        life = GetComponent<LifeSystem>();
+        if (life == null)
+        {
+            Debug.LogError("LifeSystem n'est pas trouvé sur l'objet.");
+        }
     }
 
     // Update is called once per frame
@@ -25,6 +34,7 @@ public class DamageArea : MonoBehaviour
         if (collision.transform.CompareTag("Player"))
         {
             LifeSystem.life -= 1;
+            life.UpdateLifeText();
             Debug.Log("touche");
         }
     }
