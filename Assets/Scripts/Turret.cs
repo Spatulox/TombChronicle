5e4da582c0a74da42caba60101f69b18cacae956
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2024-01-06 11:00:15 +0100

    Update script turret

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -6,53 +6,48 @@ public class Turret : MonoBehaviour
 {
     private Transform target;
     public float range = 15f;
-
-    public string playerTag = "player";
-
+    public string playerTag = "Player";
     public Transform partToRotate;
-
     private float turnSpeed = 5f;
-    
     public float fireRate = 1f;
     private float fireCountdown = 0f;
-
     public GameObject bulletPrefab;
     public Transform firePoint;
 
-    // Use this for initialization
-    void Start () {
+    void Start()
+    {
         InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
     void UpdateTarget()
     {
-        GameObject[] ennemies = GameObject.FindGameObjectsWithTag(playerTag);
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(playerTag);
         float shortestDistance = Mathf.Infinity;
-        GameObject nearestplayer = null;
+        GameObject nearestPlayer = null;
 
-        foreach (GameObject player in ennemies)
+        foreach (GameObject player in enemies)
         {
-            float distanceToplayer = Vector3.Distance(transform.position, player.transform.position);
-            if(distanceToplayer < shortestDistance)
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            if (distanceToPlayer < shortestDistance)
             {
-                shortestDistance = distanceToplayer;
-                nearestplayer = player;
+                shortestDistance = distanceToPlayer;
+                nearestPlayer = player;
             }
         }
 
-        if(nearestplayer != null && shortestDistance <= range)
+        if (nearestPlayer != null && shortestDistance <= range)
         {
-            target = nearestplayer.transform;
+            target = nearestPlayer.transform;
         }
         else
         {
             target = null;
         }
     }
-	
-    // Update is called once per frame
-    void Update () {
-        if(target == null)
+
+    void Update()
+    {
+        if (target == null)
         {
             return;
         }
@@ -61,8 +56,8 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
-        
-        if(fireCountdown <= 0f)
+
+        if (fireCountdown <= 0f)
         {
             Shoot();
             fireCountdown = 1 / fireRate;
@@ -70,16 +65,12 @@ public class Turret : MonoBehaviour
 
         fireCountdown -= Time.deltaTime;
     }
-    
+
     void Shoot()
     {
-        GameObject bulletpref = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Bullet bullet = bulletpref.GetComponent<Bullet>();
+        GameObject bulletGO = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
 
-        if(bullet != null)
-        {
-            bullet.Seek(target);
-        }
+       
     }
-  
 }
