e6b0917a3e552b26b54be1d803ff2549fe930919
ShHaWkK <alexandreuzan9@gmail.com>
2024-01-07 17:47:34 +0100

    Script ProximtyAudioPlayer (loading)

---
+++ Assets/Scripts/ProximityAudioPlayer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ProximityMusicPlayer : MonoBehaviour
+{
+    public AudioClip soundClip; 
+    public float volume = 1.0f; 
+    public float triggerRadius = 5.0f; 
+
+    private AudioSource audioSource;
+    private bool isPlayerNear = false;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = soundClip;
+        audioSource.volume = volume;
+        audioSource.loop = true; 
+        audioSource.playOnAwake = false; 
+        audioSource.spatialBlend = 1.0f; 
+
+        
+        audioSource.maxDistance = triggerRadius;
+
+        //  SphereCollider pour détecter la proximité du joueur
+        SphereCollider trigger = gameObject.AddComponent<SphereCollider>();
+        trigger.isTrigger = true;
+        trigger.radius = triggerRadius;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            isPlayerNear = true;
+            audioSource.Play(); 
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            audioSource.Stop(); 
+        }
+    }
+}
